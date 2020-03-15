8e2b3a6b993f8058805f9da7f4daa9a1e1cde8b3
DanielSantanoF <danielsantanof99@gmail.com>
2020-03-15 17:10:20 +0100

    añadimos enviorment a angular

--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAyAg_Nwtb6UHGDDcvBVj_tNqD9c43OXnM",
+    authDomain: "dsantanoproyectodam.firebaseapp.com",
+    databaseURL: "https://dsantanoproyectodam.firebaseio.com",
+    projectId: "dsantanoproyectodam",
+    storageBucket: "dsantanoproyectodam.appspot.com",
+    messagingSenderId: "763684566651",
+    appId: "1:763684566651:web:553485187b233764dbd4de",
+    measurementId: "G-CSTKV55MPX"
+  }
 };
--- Angular/src/environments/environment.ts
+++ Angular/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAyAg_Nwtb6UHGDDcvBVj_tNqD9c43OXnM",
+    authDomain: "dsantanoproyectodam.firebaseapp.com",
+    databaseURL: "https://dsantanoproyectodam.firebaseio.com",
+    projectId: "dsantanoproyectodam",
+    storageBucket: "dsantanoproyectodam.appspot.com",
+    messagingSenderId: "763684566651",
+    appId: "1:763684566651:web:553485187b233764dbd4de",
+    measurementId: "G-CSTKV55MPX"
+  }
 };
 
 /*
