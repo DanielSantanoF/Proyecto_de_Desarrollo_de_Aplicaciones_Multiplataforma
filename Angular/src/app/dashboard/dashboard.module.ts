99647111bbd0e4e9ef54fd7f18e4cddde43e67fd
DanielSantanoF <danielsantanof99@gmail.com>
2020-03-15 17:13:17 +0100

    añadimos firebase a a parte de dashboard

--- Angular/src/app/dashboard/dashboard.module.ts
+++ Angular/src/app/dashboard/dashboard.module.ts
@@ -15,6 +15,13 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { RouterModule } from '@angular/router';
+//FIREBASE
+//npm install @angular/fire firebase --save
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   imports: [
@@ -28,7 +35,12 @@ import { RouterModule } from '@angular/router';
     MatMenuModule,
     ChartsModule,
     NgxDatatableModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    //FIRE BASE
+    AngularFireModule.initializeApp(environment.firebase, 'ProyectoDAM'), // imports firebase/app needed for everything
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
   ],
   declarations: [DashboardComponent]
 })
